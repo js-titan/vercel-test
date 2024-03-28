371123b43609795729a6220ea8a788c2160cc505
Js Titan <work.shehryarahmed@gmail.com>
2024-03-28 15:06:12 +0500

    fix for DB

--- pages/api/backup.ts
+++ pages/api/backup.ts
@@ -1,42 +1,36 @@
 // pages/api/backup.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Pool } from 'pg';
-import { exec as execCb } from 'child_process';
 import fs from 'fs';
 import archiver from 'archiver';
 import dotenv from 'dotenv';
-import { promisify } from 'util';
 
 dotenv.config();
 
+import { exec as execCb } from 'child_process';
+import { promisify } from 'util';
+
 const exec = promisify(execCb);
 
-async function createDatabaseBackup(pool: Pool, dumpFile: string): Promise<void> {
-  const { POSTGRES_USER, POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_PASSWORD } = process.env;
+async function createDatabaseBackup(dumpFile: string): Promise<void> {
+  const { POSTGRES_USER, POSTGRES_DATABASE, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_PORT } = process.env;
   
   if (!POSTGRES_USER || !POSTGRES_DATABASE || !POSTGRES_HOST || !POSTGRES_PASSWORD) {
     throw new Error('Database environment variables are not fully set');
   }
 
-  const dumpCommand = `PGPASSWORD=${POSTGRES_PASSWORD} pg_dump -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} -f ${dumpFile}`;
+  const dumpCommand = `PGPASSWORD=${POSTGRES_PASSWORD} pg_dump -h ${POSTGRES_HOST} -p ${POSTGRES_PORT || '5432'} -U ${POSTGRES_USER} -d ${POSTGRES_DATABASE} -f ${dumpFile}`;
   await exec(dumpCommand);
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const pool = new Pool({
-      user: process.env.POSTGRES_USER,
-      host: process.env.POSTGRES_HOST,
-      database: process.env.POSTGRES_DATABASE,
-      password: process.env.POSTGRES_PASSWORD,
-      port: parseInt(process.env.POSTGRES_PORT || '5432'),
-    });
+   
 
     const backupFileName = 'database_backup.sql';
     const zipFileName = 'database_backup.zip';
 
-    await createDatabaseBackup(pool, backupFileName);
+    await createDatabaseBackup(backupFileName);
 
     // Ensure the backup file was created and has content
     if (!fs.existsSync(backupFileName) || fs.statSync(backupFileName).size === 0) {
